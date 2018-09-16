d97372ec82de26490dab6cde3070f6555a9f29cc
Rohin Galhotra <jiminycracken@gmail.com>
2018-09-16 09:30:12 -0700

    added controller
    objects are spawned whenever the mouse is clicked, on screen

--- Kaiju Defense Sim/Assets/spawnObject.cs
+++ Kaiju Defense Sim/Assets/spawnObject.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// modified from source code provided by TheFlyingKeyboard
+// licensed under MIT license, © 2017 TheFlyingKeyboard
+// theflyingkeyboard.net
 public class spawnObject : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+    public GameObject objectToSpawn;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetMouseButtonDown(0)) {
+            Vector3 spawnPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            spawnPosition.z = 0.0f;
+            GameObject objectInstance = Instantiate(objectToSpawn, spawnPosition, Quaternion.Euler(new Vector3(0, 0, 0)));
+        }
+    }
 }
